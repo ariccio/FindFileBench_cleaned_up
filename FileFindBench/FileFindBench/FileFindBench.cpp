8bbd52948d0b0d0630af45e94c5842a874bac1ba
Alexander Riccio <test35965@gmail.com>
2018-02-23 16:22:24 -0500

    simplified further

--- FileFindBench/FileFindBench/FileFindBench.cpp
+++ FileFindBench/FileFindBench/FileFindBench.cpp
@@ -1,15 +1,6 @@
 #include "stdafx.h"
 #include "FileFindBench.h"
 
-#define TRACE_OUT_C_STYLE( x, fmt_spec ) ::wprintf( L"\r\n\t\t" L#x L" = `" L#fmt_spec L"` ", ##x )
-
-__int64 descendDirectory( _In_ const WIN32_FIND_DATA fData, _In_ const std::wstring& normDirString) {
-	std::wstring newSzDir( normDirString );//MUST operate on copy!
-	newSzDir += L"\\";
-	newSzDir += fData.cFileName;
-	return stdRecurseFindFutures( newSzDir );
-	}
-
 bool should_remove(const WIN32_FIND_DATA& fData) {
 	if (fData.dwFileAttributes & FILE_ATTRIBUTE_DEVICE)
 		return true;
@@ -17,18 +8,10 @@ bool should_remove(const WIN32_FIND_DATA& fData) {
 		return true;
 	if (fData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN)
 		return true;
-	if (fData.dwFileAttributes & FILE_ATTRIBUTE_NO_SCRUB_DATA)
-		return true;
-	if (fData.dwFileAttributes & FILE_ATTRIBUTE_OFFLINE)
-		return true;
-	if (fData.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT)
-		return true;
-	if (fData.dwFileAttributes & FILE_ATTRIBUTE_VIRTUAL)
-		return true;
 	return false;
 	}
 
-__int64 stdRecurseFindFutures( _In_ std::wstring dir ) {
+__int64 stdRecurseFindFutures( std::wstring dir ) {
 	__int64 num = 0;
 	const std::wstring normDirString( dir );
 	assert( dir.size( ) > 2 );
@@ -45,15 +28,12 @@ __int64 stdRecurseFindFutures( _In_ std::wstring dir ) {
 	if ( fDataHand != INVALID_HANDLE_VALUE ) {
 		BOOL res = ::FindNextFileW( fDataHand, &fData );
 		while ( ( fDataHand != INVALID_HANDLE_VALUE ) && ( res != 0 ) ) {
-			const int scmpVal = ::wcscmp( fData.cFileName, L".." );
-			if ( scmpVal != 0 ) {
+			const int not_this_directory = ::wcscmp( fData.cFileName, L".." );
+			if ( ( not_this_directory != 0 ) && ( !should_remove(fData) ) ) {
 				++num;
 				}
-			if ( ( fData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) && ( scmpVal != 0 ) ) {
-				futureDirs.emplace_back( std::async( std::launch::async|std::launch::deferred, descendDirectory, fData, normDirString) );
-				}
-			else if ( ( scmpVal != 0 ) && should_remove(fData) ) {
-				--num;
+			if ( ( fData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) && ( not_this_directory != 0 ) ) {
+				futureDirs.emplace_back( std::async( std::launch::async|std::launch::deferred, stdRecurseFindFutures, ( normDirString + L"\\" + fData.cFileName ) ) );
 				}
 			res = ::FindNextFileW( fDataHand, &fData );
 			}
@@ -65,26 +45,21 @@ __int64 stdRecurseFindFutures( _In_ std::wstring dir ) {
 	return num;
 	}
 
-void startWorkAsync( _In_ std::wstring arg ) {
-	wprintf( L"Working on: `%s`\r\n", arg.c_str( ) );
-	arg = L"\\\\?\\" + arg;
-	const __int64 numberFiles = stdRecurseFindFutures( arg );
-	wprintf( L"\r\nNumber of items: %I64d\r\n", numberFiles );
-	}
-
 const DOUBLE getAdjustedTimingFrequency( ) {
 	LARGE_INTEGER timingFrequency = { 0 };
 	(void)::QueryPerformanceFrequency( &timingFrequency );
 	return ( ( DOUBLE( 1.00 ) / DOUBLE( timingFrequency.QuadPart ) ) );
 	}
 
-DOUBLE iterate( _In_ const std::wstring& arg) {
+DOUBLE iterate( const std::wstring& arg) {
 	LARGE_INTEGER startTime = { 0 };
 	LARGE_INTEGER endTime = { 0 };	
 	const DOUBLE adjustedTimingFrequency = getAdjustedTimingFrequency( );
 
 	(void)::QueryPerformanceCounter( &startTime );
-	startWorkAsync( arg );
+	wprintf(L"Working on: `%s`\r\n", arg.c_str());
+	const __int64 numberFiles = stdRecurseFindFutures(L"\\\\?\\" + arg);
+	wprintf(L"\r\nNumber of items: %I64d\r\n", numberFiles);
 	(void)::QueryPerformanceCounter( &endTime );
 
 	const DOUBLE totalTime = ( endTime.QuadPart - startTime.QuadPart ) * adjustedTimingFrequency;
--- FileFindBench/FileFindBench/FileFindBench.h
+++ FileFindBench/FileFindBench/FileFindBench.h
@@ -3,7 +3,4 @@
 #include "stdafx.h"
 #include "resource.h"
 
-__int64 stdRecurseFindFutures( _In_ std::wstring dir );
 
-const DOUBLE getAdjustedTimingFrequency( );
-// TODO: reference additional headers your program requires here
