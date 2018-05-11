5d1969c3fc32889599ecaf5779a05067ade2e877
Alexander Riccio <test35965@gmail.com>
2018-05-11 01:02:31 -0400

    Revert "lets try threadpools again"
    This reverts commit cf3e357fe964ea6cf644227756dcebfb4d971de2.

--- FileFindBench/FileFindBench/FileFindBench.h
+++ FileFindBench/FileFindBench/FileFindBench.h
@@ -154,7 +154,6 @@ struct NtdllWrap {
 //      FILE_OBJECTID_INFORMATION
 //
 
-#pragma pack(push, 1)
 typedef struct _FILE_DIRECTORY_INFORMATION {
     ULONG NextEntryOffset;
     ULONG FileIndex;
@@ -346,8 +345,6 @@ typedef struct _REPARSE_DATA_BUFFER {
         } GenericReparseBuffer;
     } DUMMYUNIONNAME;
 } REPARSE_DATA_BUFFER, *PREPARSE_DATA_BUFFER;
-
-#pragma pack(pop)
 
 #if _MSC_VER >= 1200
 #pragma warning(pop)
